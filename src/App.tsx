e15ea9b2a6a41ff8c2ff8e23075ef849ea6dc7aa
webInRoad <1040233545@qq.com>
2021-04-12 20:59:41 +0800

    useRef与createRef的区别

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,17 @@ import './styles.css'
 // import LinkButton from "./ref/useRef1";
 // import LinkButton from './ref/globalFix1'
 // import LinkButton from './ref/globalFix2'
-import LinkButton from './ref/differenceFix1and2'
+// import LinkButton from './ref/differenceFix1and2'
+import LinkButton from './ref/useRefAndCreateRef'
 export default function App() {
 	return (
 		<div className="App">
 			<LinkButton />
-			<br />
+			{/* <br />
 			<LinkButton />
 			<br />
 			<LinkButton />
-			<br />
+			<br /> */}
 		</div>
 	)
 }
